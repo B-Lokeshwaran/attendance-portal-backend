41305279503a9019a36b30c9663c58481b67d734
B-Lokeshwaran <98377427+B-Lokeshwaran@users.noreply.github.com>
2022-03-15 16:35:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ module.exports = {
   db: null,
   async connect() {
     try {
-      let con = await MongoClient.connect("mongodb+srv://lokeshwaran:lokesh78@cluster-1.wd2yz.mongodb.net?retryWrites=true&w=majority");
+      let con = await MongoClient.connect("mongodb+srv://toor:toor@cluster0.mfvku.mongodb.net?retryWrites=true&w=majority");
       this.db = con.db('attendance-portal');
       console.log("---MongoDB Connected---");
     } catch (err) {
